dee1b50a4bfcac2fa67b1a0f0dbe08cc4390f2cf
Paul-developpeur <paulallebee@gmail.com>
2024-01-17 17:33:38 +0100

    feat: add header

--- main.go
+++ main.go
@@ -12,6 +12,28 @@ func main() {
 
 	// Find and visit all links
 	c.OnHTML("table#top20", func(table *colly.HTMLElement) {
+		table.ForEach("thead", func(_ int, row *colly.HTMLElement) {
+			// Initialize a variable to store the concatenated values
+			var result string = ""
+
+			// Iterate over each cell (td) in the row
+			row.ForEach("th", func(_ int, th *colly.HTMLElement) {
+				// Concatenate the text value with a space and a dash
+				row.ForEach("th:nth-child(3)", func(_ int, th *colly.HTMLElement) {
+					th.Text = ""
+				})
+				if th.Text == "" {
+					result += ""
+				} else {
+					result += th.Text + " - "
+				}
+
+			})
+
+			// Remove the trailing space and dash, then print the result
+			fmt.Println(strings.TrimSuffix(result, " - "))
+		})
+
 		// Iterate over each row in the table
 		table.ForEach("tr", func(_ int, row *colly.HTMLElement) {
 			// Initialize a variable to store the concatenated values
@@ -23,13 +45,13 @@ func main() {
 				if td.Text == "" {
 					result += ""
 				} else {
-					result += td.Text + " - "
+					result += td.Text + "	- 	"
 				}
 
 			})
 
 			// Remove the trailing space and dash, then print the result
-			fmt.Println(strings.TrimSuffix(result, " - "))
+			fmt.Println(strings.TrimSuffix(result, ""))
 		})
 	})
 
